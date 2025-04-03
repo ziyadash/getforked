059042f090db8025e869e69390e9cbdb32035162
Brandan <brandanrudnick@gmail.com>
2025-04-03 14:17:56 +1100

    fixed formating

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -4,15 +4,13 @@ import MainButton from '../components/buttons/MainButton'
 import StyledBackground from '../components/background/StyledBackground'
 
 export default function MainPage() {
-  return (
-    <StyledBackground className='main'>
-
-        <h1 className="title">Electus</h1>
-        <div className="button-container">
-            <MainButton action="Create Vote"/>
-            <MainButton action="Join Vote"/>
-
-        </div>
-    </StyledBackground>
-  )
+    return (
+        <StyledBackground className='main'>
+            <h1 className="title">Electus</h1>
+            <div className="button-container">
+                <MainButton action="Create Vote"/>
+                <MainButton action="Join Vote"/>
+            </div>
+        </StyledBackground>
+    )
 }
