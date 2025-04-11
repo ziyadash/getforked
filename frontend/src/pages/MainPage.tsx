ae918c92d346300f3499d7020f1103dccc6e2bda
Catherine <z5359881@student.unsw.edu.au>
2025-04-11 12:49:49 +1000

    logo and favicon update

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,11 +1,16 @@
 import './MainPage.css'
 import MainButton from '../components/buttons/MainButton'
 import StyledBackground from '../components/background/StyledBackground'
+import logo from '../assets/logo.png';
 
 export default function MainPage() {
     return (
         <StyledBackground className='main'>
-            <h1 className="title">Electus</h1>
+            <div className='flex flex-row'>
+                <img className='mr-[20px]' src={logo} alt="electus logo" width="55" />
+                <h1 className="title">Electus</h1>
+            </div>
+
             <div className="button-container">
                 <MainButton action="Join Vote" />
                 <MainButton action="Create Vote" />
