79daa8ba5411c6ff3f8a08ff222e6ee3d0071e58
Catherine <z5359881@student.unsw.edu.au>
2025-04-11 11:51:36 +1000

    imported tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(), svgr(), tailwindcss(),
+  ],
 })
