fdc1ba4f8ae024c2a48058a04b8918a0d674e833
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2025-04-26 19:48:29 +1000

    add QuestionType to Question interface

--- shared/interfaces.ts
+++ shared/interfaces.ts
@@ -3,11 +3,11 @@ interface Voter {
     username: string,
 }
 
-interface Location {
-    name: string,
-    latitude: number,
-    longitude: number,
-}
+// interface Location {
+//     name: string,
+//     latitude: number,
+//     longitude: number,
+// }
 
 interface Image {
     name: string,
@@ -16,16 +16,16 @@ interface Image {
 }
 
 interface Election {
+    id: number,
+    authUserZId: string, // the owner of the election
     name: string,
     description: string,
     images: string[]
     // Optional | Remote
-    location?: Location,
+    location?: string,
     date_time_start: Date,
     date_time_end: Date,
     requires_zid: boolean,
-
-    // Positions
     questions: Question[]
 } 
 
@@ -36,9 +36,12 @@ enum QuestionType {
 
 interface Question {
     //Title e.g. Treasurer
+    id: number;
     title: string
     candidates: Candidate[],
     vote_answers: VoteAnswer[],
+    questionType: QuestionType,
+    QuestionType: QuestionType
 }
 
 // Change this to use ZK Proof 
@@ -48,6 +51,41 @@ interface VoteAnswer {
 }
 
 interface Candidate {
-    fullname: string,
-    zid?: string,
+    zId?: number;
+    fullName: string;
+    description: string;
+    image: string;
+    votes: number;
+}
+export {
+    Voter,
+    // Location,
+    Image,
+    Election,
+    Question,
+    QuestionType,
+    VoteAnswer,
+    Candidate,
+}
+
+export interface User {
+    name: string, 
+    zId: string,
+};
+
+// this is a generic session
+// we will discuss more design later but it makes sense to have 
+// different kinds of sessions maybe for voting vs creating votes?
+export interface Session {
+    sessionId: string,
+    userZId: string,
+}
+
+export interface DataStore {
+    users: User[],
+    elections: Election[],
+}
+
+export interface SessionStore {
+    sessions: Session[]
 }
