66b31b3189a69eda9e1e30730cf8a31eb8c17920
Patrick Tang Truong <z5549057@ad.unsw.edu.au>
2025-04-29 17:10:04 +1000

    test works, but failing test

--- backend/src/authRoutes.test.ts
+++ backend/src/authRoutes.test.ts
@@ -4,19 +4,20 @@ import app from '../src/app'
 describe('POST /createVoteSession', () => {
   it('Should create a vote session successfully', async () => {
     const res = await request(app)
-    .post('/createVoteSession')
+    .post('/api/auth/createVoteSession')
     .set('x-session-id', 'validSession123')
     .send({
+      userSessionId: "asdasdlkj",
       title: "Test Election",
       description: "this is a test elec",
       images:[],
       startDate: new Date(),
       endDate: new Date(),
-      zid_requirements: false,
+      zid_requirement: false,
       locationOfVote: "library"
     });
 
     expect(res.statusCode).toEqual(200);
-    expect(res.body.result).toBeDefined;
+    expect(res.body.result).toBeDefined();
   })
 })
