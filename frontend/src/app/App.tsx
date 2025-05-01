d918112bf18666f9e56a1f695104efd0b748ff1e
Catherine <z5359881@student.unsw.edu.au>
2025-05-01 12:46:29 +1000

    set up organiser page file

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import MainPage from '../pages/MainPage'
 import VoterPage from '../pages/VoterPage'
 import ManagerPage from '../pages/ManagerPage'
+import OrganiserPage from '../pages/OrganiserPage'
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/voter" element={<VoterPage />} />
                 <Route path="/manager" element={<ManagerPage />} />
+                <Route path="/organiser" element={<OrganiserPage />} /> {/* template */}
             </Routes>
         </BrowserRouter>
     )
---
+++ frontend/src/pages/OrganiserPage.tsx
@@ -0,0 +1,11 @@
+import StyledBackground from "../components/background/StyledBackground";
+import Banner from "../components/logo/Banner";
+
+export default function OrganiserPage() {
+  return (
+    <StyledBackground className='main'>
+      <Banner />
+      {/* <AuthBox /> */}
+    </StyledBackground>
+  )
+}
