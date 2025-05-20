f495fce8efe58bd082ae6adc0be804c08132bce8
Louis Diep <louisdiepp@gmail.com>
2025-05-20 22:47:19 +1000

    add ordering and delete functionality to AddPositions

--- frontend/src/pages/AddPositionsPage.tsx
+++ frontend/src/pages/AddPositionsPage.tsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import StyledBackground from "../components/background/StyledBackground";
 import WideButton from "../components/buttons/WideButton";
 import Heading from "../components/buttons/Heading";
 import SmallButton from "../components/buttons/SmallButton";
 import WideAddButton from "../components/buttons/WideAddButton";
+import { deleteElement, reorderElements } from "./helpers";
 
 export default function AddPositionsPage() {
-    const buttons = ['up', 'down']
-    const votingSessions = [
+    const [positions, setPositions] = useState([
         'Treasurer', 
         'GEDI Officer', 
         'Admin Officer',
         'Co-President (Project Operations)',
-    ]
+    ]);
+
+    const handleReorder = (index: number, direction: 'up' | 'down') => {
+        setPositions(reorderElements(positions, index, direction));
+    };
+
+    const handleDeletion = (index: number) => {
+        const newPositions = [...positions];
+        newPositions.splice(index, 1);
+        setPositions(deleteElement(positions, index));
+    }
 
     return (
         <StyledBackground className='main'>
@@ -24,16 +35,21 @@ export default function AddPositionsPage() {
                 p-[4rem]
             ">
                 <Heading text="Add Positions"/>
-                {votingSessions.map((name) => (
+                {positions.map((name, index) => (
                     <div className="flex flex-row justify-center items-center gap-[2vw]">
                         <WideButton text={name} margin="mt-[0]">
                             <div className="buttons-container">
-                                {buttons.map((type) => (
-                                    <SmallButton buttonType={type} />
-                                ))}
+                                <SmallButton 
+                                    buttonType="up" 
+                                    onClick={() => handleReorder(index, 'up')} 
+                                />
+                                <SmallButton 
+                                    buttonType="down" 
+                                    onClick={() => handleReorder(index, 'down')} 
+                                />
                             </div>
                         </WideButton>
-                        <SmallButton buttonType="bin"/>
+                        <SmallButton buttonType="bin" onClick={() => handleDeletion(index)}/>
                     </div>
                 ))}
                 <WideAddButton></WideAddButton>
