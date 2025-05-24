2a6a409ad528f5c06b1bb330ad3911a0f44330ee
Catherine <z5359881@student.unsw.edu.au>
2025-05-24 22:51:32 +1000

    linting / spacing

--- frontend/src/components/buttons/WideButton.tsx
+++ frontend/src/components/buttons/WideButton.tsx
@@ -3,7 +3,7 @@ import './WideButton.css'
 interface WideButtonInputs {
 	text?: string,
 	margin?: string,
-    children?: React.ReactNode
+	children?: React.ReactNode
 }
 
 export default function WideButton({ text, margin, children }: WideButtonInputs) {
@@ -11,9 +11,9 @@ export default function WideButton({ text, margin, children }: WideButtonInputs)
 		<div className={`flex justify-around ${margin}`}>
 			<div className="wide-button">
 				<div className="ml-10 max-h-full overflow-y-auto text-left">{text}</div>
-                <div className="buttons">
-                    {children}
-                </div>
+				<div className="buttons">
+					{children}
+				</div>
 			</div>
 		</div>
 	);
