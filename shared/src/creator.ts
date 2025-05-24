c0a1e031c5be509c66a523d40a1f02ee56bb8eae
Louis Diep <louisdiepp@gmail.com>
2025-05-24 15:40:03 +1000

    add function stubs

---
+++ shared/src/creator.ts
@@ -0,0 +1,55 @@
+/**
+ * Register a vote creator
+ */
+
+/**
+ * Login a vote creator
+ */
+
+/** 
+ * List vote sessions of a vote creator
+ */
+
+/**
+ * Get details of a specific vote session that they have created
+ * should include all positions and candidates
+ */
+
+/**
+ * Create a new vote with basic details inc.
+ * - vote name
+ * - location
+ * - date/time
+ * - description
+ * - other params in the figma/frontend are disabled
+ */
+
+/**
+ * Edit an existing vote with same details as above
+ */
+
+/**
+ * Add a position to an existing vote with details inc.
+ * - Position name
+ * - Voting type
+ * - Candidates (no functions to edit a candidate (this functionality will be disabled))
+ */
+
+/**
+ * Add a candidate to a position
+ */
+
+/**
+ * Delete a candidate from a position
+ */
+
+/**
+ * Reorder positions
+ */
+
+/**
+ * Delete a position
+ */
+
+
+
---
+++ shared/src/election.ts
@@ -0,0 +1,25 @@
+/**
+ * Get election status. States: Ready to start | In progress | Ended
+ * An election is ready to start if it has at least one position
+ */
+
+/**
+ * Start an election - should return a code to join the election
+ * This function is called once authorised votes have been added to the election.
+ */
+
+/**
+ * Add zId to authorised voters in election
+ */
+
+/**
+ * Delete zId from authorised votes in election
+ */
+
+/**
+ * End an election
+ */
+
+/**
+ * Get results of an election. Election must have ended
+ */
